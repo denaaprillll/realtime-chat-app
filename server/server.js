d37717699d34d434c62ff661ed992881d30fba4f
Dena April <denaapril1504@gmail.com>
2026-06-25 21:43:58 +0700

    Membuat chat realtime sederhana

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server Running");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
