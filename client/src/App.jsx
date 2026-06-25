62f3898a35869acce11eabcde75b5265e23cf4cc
Dena April <denaapril1504@gmail.com>
2026-06-25 21:51:38 +0700

    Menambahkan fitur username pada chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,21 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:3000");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [joined, setJoined] = useState(false);
+
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
   const sendMessage = () => {
     if (message.trim() === "") return;
 
-    socket.emit("send_message", message);
+    const data = {
+      username,
+      message,
+    };
+
+    socket.emit("send_message", data);
     setMessage("");
   };
 
@@ -24,13 +32,40 @@ function App() {
     };
   }, []);
 
+  if (!joined) {
+    return (
+      <div style={{ padding: "20px" }}>
+        <h1>Masuk Chat</h1>
+
+        <input
+          type="text"
+          placeholder="Masukkan nama..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <button
+          onClick={() => {
+            if (username.trim() !== "") {
+              setJoined(true);
+            }
+          }}
+        >
+          Gabung Chat
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Realtime Chat App</h1>
 
       <div>
         {messages.map((msg, index) => (
-          <p key={index}>{msg}</p>
+          <p key={index}>
+            <strong>{msg.username}:</strong> {msg.message}
+          </p>
         ))}
       </div>
 
